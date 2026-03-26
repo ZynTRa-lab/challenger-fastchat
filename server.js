db5178be238ffc8d51a287642792ae1046576474
ZynTRa-lab <egeeevatansvr@gmail.com>
2026-03-26 23:22:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,9 +12,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
     console.log('Bir kullanıcı bağlandı!');
 
-    // Mesaj geldiğinde herkese gönder
+    // Genel mesajlaşma
     socket.on('chat message', (data) => {
         io.emit('chat message', data); 
+    });
+
+    // Arkadaşlık isteği gönderildiğinde
+    socket.on('send friend request', (data) => {
+        // Şimdilik herkese duyurur (Önemli: Alıcıya ulaşması için)
+        socket.broadcast.emit('receive friend request', data); 
+    });
+
+    // Arkadaşlık isteğine cevap verildiğinde
+    socket.on('respond friend request', (data) => {
+        socket.broadcast.emit('request response', {
+            from: data.to, 
+            accepted: data.accepted
+        });
     });
 
     socket.on('disconnect', () => {
@@ -24,5 +38,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Sunucu ${PORT} portunda çalışıyor.`);
+    console.log(`Sunucu ${PORT} portunda hazır!`);
 });
