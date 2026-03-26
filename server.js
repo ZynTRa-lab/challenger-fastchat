361ceb0136f33b9b561d0cf24fdbba8cf3a03a58
ZynTRa-lab <egeeevatansvr@gmail.com>
2026-03-26 22:52:05 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('Bir kullanıcı bağlandı!');
+
+    // Mesaj geldiğinde herkese gönder
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data); 
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Bir kullanıcı ayrıldı.');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor.`);
+});
